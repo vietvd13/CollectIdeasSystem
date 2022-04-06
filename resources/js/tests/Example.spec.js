d2448907979551d7360ba5aabe4713a8b45dacce
Vu Duc Viet <vuducviet0131@gmail.com>
2022-04-06 13:42:25 +0700

    [Jest][Setup]

---
+++ resources/js/tests/Example.spec.js
@@ -0,0 +1,5 @@
+describe('Example', () => {
+	it('Case 1', () => {
+		expect(1 + 1).toEqual(2);
+	});
+});
---
+++ resources/js/tests/setup.js
@@ -0,0 +1,8 @@
+// import Vue from 'vue';
+// import { config } from '@vue/test-utils';
+// require('dotenv').config();
+
+// import BootstrapVue from 'bootstrap-vue';
+// Vue.use(BootstrapVue);
+
+// config.mocks.$t = key => key;
