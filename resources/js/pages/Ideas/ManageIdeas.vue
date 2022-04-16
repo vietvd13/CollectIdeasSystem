2581ee1f9ba71b7aaf49f1b15e82c5f106ff11bc
Vu Duc Viet <vuducviet0131@gmail.com>
2022-04-16 11:56:31 +0700

    [FE][Fix style]

--- resources/js/pages/Ideas/ManageIdeas.vue
+++ resources/js/pages/Ideas/ManageIdeas.vue
@@ -90,7 +90,7 @@
 										</div>
 									</div>
 								</b-col>
-								<b-col cols="3" sm="12" md="3" lg="3" xl="3">
+								<b-col cols="6" sm="6" md="4" lg="4" xl="4">
 									<div class="d-flex flex-column justify-content-center">
 										<div
 											:class="{
@@ -105,7 +105,7 @@
 										</div>
 									</div>
 								</b-col>
-								<b-col cols="3" sm="12" md="3" lg="3" xl="3">
+								<b-col cols="12" sm="12" md="4" lg="4" xl="4">
 									<div class="d-flex flex-column justify-content-center">
 										<div
 											@click="showModal(true, idea.id)"
